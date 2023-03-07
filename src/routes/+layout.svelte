730ef095fce43df26c5f54835320fb62b1e3c81c
ericyork <eyork@clarkson.edu>
2023-03-07 13:07:14 -0500

    initial import

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+  import '../global.css';
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/posts">Posts</a>
+  <a href="/favs">Favs</a>
+  <a href="/info">Info</a>
+  <a href="/about">About</a>
+  <a href="/donate">Donate</a>
+</nav>
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <a href="/">&copy;2023 Eric York</a>
+</footer>
+
+<style>
+  nav, footer {
+    display: flex;
+    justify-content: space-evenly;
+    padding: .5em;
+    background-color:honeydew;
+    color: seagreen;
+  }
+  main {
+    grid-row: 2;
+    padding: 0 2rem;
+    background-color: rgba(255,255,255,.05);
+  }
+  footer {
+  grid-row: 3;
+  position: sticky;
+  bottom: 0;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<img src="/img/tabby.png" alt="a tabby cat with blue eyes">
+<h1>Welcome to Cat World!</h1>
+<p>This blog is dedicated to all things feline. Here, I explore the fascinating world of cats, both wild and domestic, and delve into their unique needs and wants. Whether you're a seasoned cat lover or simply curious about these curious creatures, you'll find plenty of interesting content here to satisfy your curiosity.</p>
+<p>From practical tips on caring for your pet cat, to in-depth discussions of the behavior and biology of wild cats, my blog covers a wide range of topics related to our feline friends. I'm passionate about providing accurate and up-to-date information, backed by scientific research and personal experience, to help cat owners and enthusiasts better understand these fascinating creatures.</p>
+<p>So whether you're looking for advice on how to keep your cat happy and healthy, or simply want to learn more about the amazing world of cats, you've come to the right place. Join me on this exciting journey of discovery, as we explore the many wonders of the feline kingdom!</p>
+
+<style>
+
+  img {
+    float: right;
+    margin-top: 8rem;
+    padding: 0 2rem;
+    max-width: 38%;
+  }
+</style>
